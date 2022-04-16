a7e369c11dd1f5a81ee6f4da5a42f1d244e3a88e
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-04-16 21:14:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'wa.me/60147366955'
-global.author = 'iYeen by Putbotz'
+global.author = 'iYeen by Putbotz🦕'
 
 global.multiplier = 999 // The higher, The harder levelup
 
